2bb6d64d26cbbe59300855b9eede1809b974cc76
Gian <gianpolastri@gmial.com>
2023-07-19 21:51:56 -0300

    feat(src/utils): testData y testDataUploader

--- api/src/utils/testDataUpload.js
+++ api/src/utils/testDataUpload.js
@@ -1,3 +1,50 @@
+const { bookGenre, eBook, Format, Language, Publisher, } = require('../db');
+const {
+    eBooksTest,
+    genreTest,
+    publisherTest,
+    languageTest,
+    formatTest,
+} = require('./testData');
+
+const testDataUploader = async () => {
+
+    try {
+
+        const eBooks = await eBook.bulkCreate( eBooksTest, { ignoreDuplicates: true });
+
+        const genre = await bookGenre.bulkCreate( genreTest, { ignoreDuplicates: true });
+        
+        const publisher = await Publisher.bulkCreate( publisherTest, { ignoreDuplicates: true });
+
+        const language = await Language.bulkCreate( languageTest, { ignoreDuplicates: true });
+
+        const format = await Format.bulkCreate( formatTest, { ignoreDuplicates: true });
+
+        await eBooks.forEach( book => book.addPublisher(Math.floor(Math.random() * 11 + 1)))
+        
+        await eBooks.forEach( book => book.addFormat(Math.floor(Math.random() * 11 + 1)))
+        
+        await eBooks.forEach( book => book.addLanguage(Math.floor(Math.random() * 11 + 1)))
+        
+        await eBooks.forEach( book => book.addGenre(Math.floor(Math.random() * 11 + 1)))
+
+
+
+        console.log('Datos cargados exitosamente!');
+
+
+
+        return;
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+}
+
 // const {
 //     Activity,
 //     ActivityType,
@@ -131,4 +178,7 @@
 
 // }
 
-// module.exports = { testDataUploader, testDataCheck };
+module.exports = {
+    testDataUploader,
+    // testDataCheck
+ };
