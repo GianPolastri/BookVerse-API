3c2a73c7c3e2e09d00594c7fdcce7a200b1b2e5e
fedezelarayan <federico.zelarayan@hotmail.com>
2023-08-08 18:47:21 -0300

    filter/order books

--- api/src/handlers/filterHandler/filterBooksHandler.js
+++ api/src/handlers/filterHandler/filterBooksHandler.js
@@ -4,7 +4,9 @@ const {
     filtroFormato,
     filtroLenguaje,
     filtroEditorial,
-    filtrarLibrosCombinados
+    filtrarLibrosCombinados,
+    porNombre,
+    porPrecio
 } = require ('../../controllers/filterControllers/filterBooksControllers');
 
 
@@ -59,16 +61,64 @@ const filterByPublisher = async (req, res) => {
 }
 
 const combinedFilters =  async (req, res) => {
-    const { title, genre, format, language, publisher } = req.query;
-  
+    const filtros = {
+        title: req.query.title || '',
+        genre: req.query.genre || '',
+        format: req.query.format || '',
+        language: req.query.language || '',
+        publisher: req.query.publisher || ''
+    }
+    const order = req.query.order || '';
+   
     try {
-      const filtros = { title, genre, format, language, publisher };
-      const librosFiltrados = await filtrarLibrosCombinados(filtros);
-      res.json(librosFiltrados);
+        let resultado;
+        if (order === 'price') {
+            resultado = await porPrecio(filtros, req.query.price);
+        } else if (order === 'name') {
+            resultado = await porNombre(filtros, req.query.name);
+        } else {
+            resultado = await filtrarLibrosCombinados(filtros, []);
+        }
+    
+        res.status(200).json(resultado);
+/*       const filtros = { title, genre, format, language, publisher };
+      const ordenamiento = { price, name }
+      const librosFiltrados = await filtrarLibrosCombinados(filtros, ordenamiento);
+      res.json(librosFiltrados); */
     } catch (error) {
       res.status(400).json({ 'error': error });
     }
 };
+
+
+/* const express = require('express');
+const router = express.Router();
+const { filtrarLibrosCombinados, porPrecio, porNombre } = require('./tu_controlador');
+
+router.get('/filter/combined', async (req, res) => {
+    const filtros = {
+        title: req.query.title || '',
+        genre: req.query.genre || '',
+        format: req.query.format || '',
+        language: req.query.language || '',
+        publisher: req.query.publisher || ''
+    };
+
+    const ordenamiento = req.query.ordenamiento || ''; // Obtén el ordenamiento desde la consulta
+
+    let resultado;
+    if (ordenamiento === 'precio') {
+        resultado = await porPrecio(filtros, req.query.precio);
+    } else if (ordenamiento === 'nombre') {
+        resultado = await porNombre(filtros, req.query.titulo);
+    } else {
+        resultado = await filtrarLibrosCombinados(filtros, []);
+    }
+
+    res.json(resultado);
+});
+
+module.exports = router; */
 
 
 module.exports = {
