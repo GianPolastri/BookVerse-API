6e7a93df132957e6ca5414011e53b7d8669c3493
GianPolastri <120734326+GianPolastri@users.noreply.github.com>
2023-08-08 17:42:09 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 const PORT =  /*process.env.PORT || */ 3001;
 const { testDataUploader, testDataCheck } = require('./src/utils/testDataUpload');
 
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
 
   const { BookCheck, publisherCheck } = await testDataCheck();
   
